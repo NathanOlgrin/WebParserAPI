6bef98774db12b61f3c436c4b7aa768e824067f1
NathanOlgrin <matrica_1997@mail.ru>
2026-04-01 20:08:34 +0300

    First commit

---
+++ src/main/java/ru/olgrin/WebParserAPI/WebParserApiApplication.java
@@ -0,0 +1,13 @@
+package ru.olgrin.WebParserAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebParserApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebParserApiApplication.class, args);
+	}
+
+}
