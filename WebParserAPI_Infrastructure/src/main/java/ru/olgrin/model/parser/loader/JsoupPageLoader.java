50b0e3657d6e6ec2020eaa31c10dc088adc8a897
NathanOlgrin <matrica_1997@mail.ru>
2026-04-04 22:45:59 +0300

    Убрана перекодировка скачивающейся html-страницы в windows-1251.

--- WebParserAPI_Infrastructure/src/main/java/ru/olgrin/model/parser/loader/JsoupPageLoader.java
+++ WebParserAPI_Infrastructure/src/main/java/ru/olgrin/model/parser/loader/JsoupPageLoader.java
@@ -6,12 +6,18 @@ import org.jsoup.Jsoup;
 public class JsoupPageLoader implements PageLoader {
     @Override
     public String load(String url) throws Exception {
-        Connection.Response response = Jsoup.connect("https://mid.ru/")
-                .userAgent("Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201")
+//        Connection.Response response = Jsoup.connect("https://mid.ru/")
+//                .userAgent("Mozilla/5.0 (Windows; U; Windows NT 6.1; rv:2.2) Gecko/20110201")
+//                .referrer("https://www.google.com/")
+//                .execute();
+//        byte[] bytes = response.bodyAsBytes();
+//        String html = new String(bytes, "windows-1251");
+//        return html;
+
+        return org.jsoup.Jsoup.connect(url)
+                .userAgent("Chrome/146.0.7680.165 Safari/537.36")
                 .referrer("https://www.google.com/")
-                .execute();
-        byte[] bytes = response.bodyAsBytes();
-        String html = new String(bytes, "windows-1251");
-        return html;
+                .get()
+                .html();
     }
 }
