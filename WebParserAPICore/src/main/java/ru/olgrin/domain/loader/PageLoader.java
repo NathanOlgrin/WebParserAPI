ec78b4bf29cd4be3a92195b5047084d139f1d5ff
NathanOlgrin <matrica_1997@mail.ru>
2026-04-04 18:46:23 +0300

    Изменение конфигурации проекта на multi-module

--- WebParserAPICore/src/main/java/ru/olgrin/domain/loader/PageLoader.java
+++
@@ -1,5 +0,0 @@
-package ru.olgrin.domain.loader;
-
-public interface PageLoader {
-    String load(String url) throws Exception;
-}
--- WebParserAPICore/src/main/java/ru/olgrin/domain/strategy/ParsingStrategy.java
+++
@@ -1,8 +0,0 @@
-package ru.olgrin.domain.strategy;
-
-import ru.olgrin.domain.config.ParserConfig;
-
-public interface ParsingStrategy {
-    boolean supports(String url);
-    ParserConfig getConfig();
-}
