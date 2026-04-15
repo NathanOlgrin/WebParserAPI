de9674989eeeebcf9e0c6da3110f481bb0cdb7f0
NathanOlgrin <matrica_1997@mail.ru>
2026-04-15 20:39:42 +0300

    Добавление интерфейса, описывающего поиск LLM ответа на вопрос в векторной БД

--- WebParserAPI_Core/src/main/java/ru/olgrin/ports/out/VectorStorePort.java
+++ WebParserAPI_Core/src/main/java/ru/olgrin/ports/out/VectorStorePort.java
@@ -1,6 +1,7 @@
 package ru.olgrin.ports.out;
 
 import ru.olgrin.ports.out.model.VectorData;
+import ru.olgrin.ports.out.model.VectorDocument;
 import ru.olgrin.ports.out.model.VectorSaveResult;
 
 import java.util.List;
@@ -8,4 +9,5 @@ import java.util.List;
 public interface VectorStorePort {
 
     VectorSaveResult save(List<VectorData> embeddingVectors);
+    List<VectorDocument> search(String query, Integer topK);
 }
