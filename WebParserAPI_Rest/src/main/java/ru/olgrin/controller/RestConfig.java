d6a70ac538a826370949528a67bacb22fad3ec5f
NathanOlgrin <matrica_1997@mail.ru>
2026-04-15 20:40:59 +0300

    Добавление ручной перекодировки в utf-8

--- WebParserAPI_Rest/src/main/java/ru/olgrin/controller/RestConfig.java
+++ WebParserAPI_Rest/src/main/java/ru/olgrin/controller/RestConfig.java
@@ -3,15 +3,26 @@ package ru.olgrin.controller;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
+
+import java.nio.charset.StandardCharsets;
 
 @Configuration
 public class RestConfig {
 
     @Bean
     public RestTemplate restTemplate() {
-        return new RestTemplateBuilder()
+        RestTemplate restTemplate = new RestTemplateBuilder()
                 .rootUri("http://localhost:8082")
                 .build();
+
+        // ФИКС КОДИРОВКИ
+        restTemplate.getMessageConverters().removeIf(c -> c instanceof StringHttpMessageConverter);
+        restTemplate.getMessageConverters().add(
+                new StringHttpMessageConverter(StandardCharsets.UTF_8)
+        );
+
+        return restTemplate;
     }
 }
